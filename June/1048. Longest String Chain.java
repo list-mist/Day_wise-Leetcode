2e880e055c0a4224dbde534e01bc9eecea095072
list-mist <87711815+list-mist@users.noreply.github.com>
2022-06-15 13:11:10 +0530

    Create 1048. Longest String Chain.java

---
+++ June/1048. Longest String Chain.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, new custom());
+        return longestChain(words);   
+    }
+    class custom implements Comparator<String>{
+        public int compare(String a, String b){
+            return a.length() - b.length();
+        }
+    }
+    int longestChain(String[] words){
+        int n = words.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        int max = 1;
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = i + 1; j < n; j++){
+                 boolean res = valid(words[i],words[j]);
+                if(res){
+                    dp[i] = Math.max(dp[i],1 + dp[j]);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+    boolean valid(String s1, String s2){
+        if(s2.length() - s1.length() != 1) return false;
+        int cnt =  0;
+        int i = 0, j = 0;
+        
+        while(i < s1.length() && j < s2.length()){
+             if(s1.charAt(i) != s2.charAt(j)){
+                cnt++;
+                j++;
+            }else{
+                 i++;
+                 j++;
+             }
+        }
+        
+        if(cnt <= 1) return true;
+        return false;
+    }
+    
+}
