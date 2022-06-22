a550f76c908e00a1d501bc0c62fc6dd43b1a0ed7
list-mist <87711815+list-mist@users.noreply.github.com>
2022-06-22 06:26:25 +0530

    Create 215. Kth Largest Element in an Array.java

---
+++ June/215. Kth Largest Element in an Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i : nums) pq.add(i);
+        int res = 0;
+        while(!pq.isEmpty() &&  k != 0){
+            res = pq.poll();
+            k--;
+        }
+        
+        return res;
+    }
+}
